7279bdfd3d93d81650074a3192f86ba8cdb2d64e
Balaji Sivaraman <balaji@balajisivaraman.com>
2018-08-04 17:47:39 +0530

    Make sure a simple Warp endpoint is wired up properly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#[macro_use] extern crate warp;
+
+use warp::Filter;
+
 fn main() {
-    println!("Hello, world!");
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 8080));
 }
